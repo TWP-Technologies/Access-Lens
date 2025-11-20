c8adc583db1fb17cf7d3e04b4eb975d18ebf5584
Knot False <33779537+KnotFalse@users.noreply.github.com>
2025-11-19 19:34:59 -0600

    ci: add release asset workflow and lock builds

--- admin/assets/js/gutenberg-integration-react/gutenberg-integration.jsx.asset.php
+++ admin/assets/js/gutenberg-integration-react/gutenberg-integration.jsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0144ea683d9333d6ec78');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8a1a38a16b784e8023e0');
